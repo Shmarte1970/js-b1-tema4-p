31b938a2a4fe5b64f9f7912006c23bc63167e9a3
Shmarte1970 <shmarte@gmail.com>
2026-03-26 09:19:54 +0100

    js-b1-tema4-p Pedro Rios

--- U4/U4EJ2.js
+++ U4/U4EJ2.js
@@ -26,6 +26,12 @@
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
 
+function fetchOctocatUsingThen() {
+  const url = 'https://api.github.com/users/octocat';
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data);
+}
 
 /**
  * TEST
